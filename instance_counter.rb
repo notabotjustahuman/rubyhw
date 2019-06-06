0333b9960b247f597596540af9d755a9e67b83f9
Billy Chen <billy.chen1206@gmail.com>
2019-06-06 12:46:14 -0400

    think this is better now

--- instance_counter.rb
+++ instance_counter.rb
@@ -1,12 +1,16 @@
 def instance_counter
   puts "Enter some words:"
   x = gets.chomp
-  .gsub(/[^a-z]/, '') # .gsub - ^a-z if it aint a letter, get bopped
-  .chars # get me those letters
-  .group_by(&:itself) # group them to itself duh
+  .gsub(/[^a-z]/, '')
+  .chars
+  .group_by(&:itself)
   .map { |letter, occurance| [letter, occurance.count] }
   .max_by(&:last)
   puts "#{x}"
 end
 
 instance_counter()
+
+# .gsub - ^a-z if it aint a letter, get bopped
+# .chars get me those letters
+# .group  group them to itself duh
