3be49a2ea362d68da8255eb9c71f3611f1eefa2d
Billy Chen <billy.chen1206@gmail.com>
2019-06-06 11:32:58 -0400

    hw woohoo

--- snake_eyes.rb
+++ snake_eyes.rb
@@ -1,9 +1,14 @@
-def snake_eyes()
-  dice1 = Random.new
-  dice2 = Random.new
-  arr = 100.times.collect { [dice1.rand(1..6), dice2.rand(1..6)] }
-  x = arr.map
+def snake_eye()
+  count = 0
+  i = 0
+  random = Random.new
+  while i <  101
+    dice1 = random.rand(1..6)
+    dice2 = random.rand(1..6)
+    if dice1 == 1 && dice2 == 1
+      count += 1
+    end
+    i += 1
+  end
+  return count
 end
-
-
-snake_eyes()
