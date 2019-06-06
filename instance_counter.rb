cc98c49517c13e973dbb326078c95b8aeb453e70
Billy Chen <billy.chen1206@gmail.com>
2019-06-06 12:42:34 -0400

    aj is god

--- instance_counter.rb
+++ instance_counter.rb
@@ -1,12 +1,12 @@
 def instance_counter
   puts "Enter some words:"
-  puts x = gets.chomp
+  x = gets.chomp
   .gsub(/[^a-z]/, '') # .gsub - ^a-z if it aint a letter, get bopped
   .chars # get me those letters
   .group_by(&:itself) # group them to itself duh
   .map { |letter, occurance| [letter, occurance.count] }
   .max_by(&:last)
-
+  puts "#{x}"
 end
 
 instance_counter()
